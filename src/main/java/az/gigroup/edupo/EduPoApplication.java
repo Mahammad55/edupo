43b9a5c6f84b54dd3fc5033142b7f29b13733e33
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-06-12 14:36:31 +0400

    Feature : Project start from zero

---
+++ src/main/java/az/gigroup/edupo/EduPoApplication.java
@@ -0,0 +1,13 @@
+package az.gigroup.edupo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduPoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/gigroup/edupo/EduPoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.gigroup.edupo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
