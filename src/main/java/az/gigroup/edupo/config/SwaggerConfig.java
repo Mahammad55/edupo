aba71d56bf0d969724aa77d08d1807909f21c656
mahammad.ilyazov <ilyazovmehemmed@gmail.com>
2024-08-02 00:39:03 +0400

    Configure swagger jwt part

--- src/main/java/az/gigroup/edupo/config/SwaggerConfig.java
+++ src/main/java/az/gigroup/edupo/config/SwaggerConfig.java
@@ -3,6 +3,12 @@ package az.gigroup.edupo.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(info = @Info(
         title = "EduPo",
@@ -14,5 +20,13 @@ import io.swagger.v3.oas.annotations.info.Info;
                 url = "https://github.com/Mahammad55/edupo"
         )
 ))
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(new Components().addSecuritySchemes("Authorization", new SecurityScheme()
+                        .name("Authorization").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
 }
